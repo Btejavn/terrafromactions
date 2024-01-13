0eb81bcae822ee309c524478d88af7f5ced8e566
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-13 15:58:10 -0500

    added

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -10,22 +10,22 @@ terraform {
     resource_group_name  = "friday-demo-rg"
     storage_account_name = "sttfstatemgt01"
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key                  = "demo.terraform.tfstate"
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  #subscription_id = var.subscription_id
+  #client_id       = var.client_id
+  #client_secret   = var.client_secret
+  #tenant_id       = var.tenant_id
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "example-resources"
+  name     = "test-rg"
   location = "West Europe"
 }
 
