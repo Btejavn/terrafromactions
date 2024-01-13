352e472ba2ead6cf86ded70067a6200c89a798c6
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-13 15:53:53 -0500

    added

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,19 +2,30 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">= 2.46.0"  # Update this version to the latest available
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "friday-demo-rg"
+    storage_account_name = "sttfstatemgt01"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "test-rg"
+  name     = "example-resources"
   location = "West Europe"
 }
+
