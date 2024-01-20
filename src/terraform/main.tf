1c9317562ed4fb6fea6d99c502794217c27b26e9
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-20 18:35:24 -0500

    addded prevention

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -7,13 +7,13 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
-  #subscription_id = var.subscription_id
-  #client_id       = var.client_id
-  #client_secret   = var.client_secret
-  #tenant_id       = var.tenant_id
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "174f8eaf-055a-4a26-b573-1c3d35723ecb"
 }
 
 # Create a resource group
