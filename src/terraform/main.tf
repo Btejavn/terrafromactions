1fd14522655934286cbf0a42193e982d0b706be5
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-20 19:56:06 -0500

    l

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -38,4 +38,16 @@ resource "azurerm_network_interface" "nic" {
   location            = var.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]  # Customize the address space as needed
+
+  subnet {
+    name           = var.subnet_name
+    address_prefix = "10.0.1.0/24"  # Customize the subnet address space as needed
+  }
+}
+
 
