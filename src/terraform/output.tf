88f5fc61a31706f11d9716e1dac8ed8825699fd7
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-20 20:14:52 -0500

    r

--- src/terraform/output.tf
+++ src/terraform/output.tf
@@ -1,17 +1,4 @@
-output "vm_name" {
-  description = "The name of the created virtual machine"
-  value       = azurerm_windows_virtual_machine.vm.name
-}
 
-output "vm_public_ip" {
-  description = "The public IP address of the virtual machine"
-  value       = azurerm_windows_virtual_machine.vm.public_ip_address
-}
-
-output "nic_id" {
-  description = "The ID of the created network interface"
-  value       = azurerm_network_interface.nic.id
-}
 
 output "resource_group_name" {
   description = "The name of the resource group"
