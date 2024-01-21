91a9250507a90412db4dbcb0e39fc6655453b4c2
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-20 20:29:24 -0500

    a

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "my_rg"
-    storage_account_name   = "mystname"
+    storage_account_name   = "mystnames"
     container_name         = "tfstate"
     key                    = "terraform.tfstate"
   }
