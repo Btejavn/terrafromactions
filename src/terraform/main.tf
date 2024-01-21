96d60c017fcb8b2d6284c1b33f30900ea44f1e55
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-20 19:52:48 -0500

    ad

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -18,4 +18,24 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  admin_password        = "Password1234!"  # Replace with your own secure password
+  network_interface_ids = var.nic
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
 
--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -7,7 +7,22 @@ variable "location" {
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "my-rg"
+  default     = "my--rg"
 }
 
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+  default     = "my-vm"
+}
 
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+variable "nic" {
+  description = "Network Interface IDs"
+  type        = list(string)
+  default     = ["my-nic"]
+}
